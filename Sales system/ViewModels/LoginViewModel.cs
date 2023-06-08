87ed2acdbcb53dff62b6e0c12dff8b49f162d6a9
Gabriel Vieira <gabrielvieira1@protonmail.com>
2023-06-08 19:41:36 -0300

    Creating the procedures to perform data links

---
+++ Sales system/ViewModels/LoginViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Sales_system.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+using Sales_system.Library;
+
+namespace Sales_system.ViewModels
+{
+    public class LoginViewModel : UserModel
+    {
+        private ICommand _command;
+        private TextBox _textBoxEmail;
+        private PasswordBox _textBoxPass;
+        private string date = DateTime.Now.ToString("dd/MM/yyy");
+        private Frame rootFrame = Window.Current.Content as Frame;
+        public LoginViewModel(object[] campos)
+        {
+            _textBoxEmail = (TextBox)campos[0];
+            _textBoxPass = (PasswordBox)campos[1];
+        }
+
+        public ICommand IniciarCommand
+        {
+            get
+            {
+                return _command ?? (_command = new CommandHandler(async () =>
+                {
+                    await IniciarAsync();
+                }));
+            }
+        }
+
+        private async Task IniciarAsync()
+        {
+            var data = Email;
+        }
+    }
+}
